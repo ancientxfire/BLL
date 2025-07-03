d0cf46b0c9717cbd7229e2bb93634f9bdc475285
Matthias Koplin <work@matthiaskoplin.me>
2025-07-03 20:39:35 +0200

    Update runner.py

--- algo/runner.py
+++ algo/runner.py
@@ -14,14 +14,10 @@ def algoRunner(alleProjekte: list, alleSuS:list, ):
     schülerTopf, projekteTopf = erstesAufteilen(alleProjekte, alleSuS, projekteTopf)
     projekteTopf, schülerTopf = projekteReduzieren(alleProjekte, alleSuS, projekteTopf, schülerTopf)
     schülerTopf = shuffleList(schülerTopf)
-    print("Vor 2",len(schülerTopf))
     projekteTopf, schülerTopf = nachNterWahlSortieren(alleProjekte, alleSuS, projekteTopf, schülerTopf, 1)
     schülerTopf = shuffleList(schülerTopf)
-    print("Vor 3",len(schülerTopf))
     projekteTopf, schülerTopf = nachNterWahlSortieren(alleProjekte, alleSuS, projekteTopf, schülerTopf, 2)
-    print("Länge Schjülertopf",len(schülerTopf))
     projekteTopf, schülerTopf = restlicheSuSaufGruppenAufteilen(alleProjekte, alleSuS, projekteTopf, schülerTopf)
-    print("Länge Schjülertopf",len(schülerTopf))
     if len(schülerTopf) > 0:
         raise ValueError('Es sind noch SuS übrig!!')
     return projekteTopf
